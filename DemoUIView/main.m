2553ced46fed5083d1d05a90db171ba853fcd7ae
duphungcong <duphungcong@gmail.com>
2015-08-09 17:55:52 +0700

    demo slider

---
+++ DemoUIView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoUIView
+//
+//  Created by du phung cong on 8/9/15.
+//  Copyright (c) 2015 duphungcong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
